b77f4877e4961d9f321fcafe15e779fde72346e4
wahyuwibowo <wahyuwibowo@mdd.co.id>
2021-08-01 12:18:09 +0700

    update image responsive

--- webpack.common.js
+++ webpack.common.js
@@ -69,9 +69,9 @@ module.exports = {
 				{
 					from: path.resolve(__dirname, 'src/public/'),
 					to: path.resolve(__dirname, 'dist/'),
-					// globOptions: {
-					// 	ignore: ['**/images/**'],
-					// },
+					globOptions: {
+						ignore: ['**/images/**'],
+					},
 				},
 			],
 		}),
